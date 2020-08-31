512d8a1c0494d7d297f909e07307b1cccfc531d5
丁侃 <657483726@qq.com>
2020-08-31 19:45:22 +0800

    Create

---
+++ DKPlayer/DKPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DKPlayer
+//
+//  Created by 丁侃 on 2020/8/31.
+//  Copyright © 2020 丁侃. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DKPlayer/DKPlayerTests/DKPlayerTests.m
@@ -0,0 +1,37 @@
+//
+//  DKPlayerTests.m
+//  DKPlayerTests
+//
+//  Created by 丁侃 on 2020/8/31.
+//  Copyright © 2020 丁侃. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DKPlayerTests : XCTestCase
+
+@end
+
+@implementation DKPlayerTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
